47bc3ad4f40f7e80146dfab3e3602b99cadb0996
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2022-07-01 00:47:40 +0600

    added mongodb uri

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000
 
 app.use(cors());
 app.use(express.json());
+
+// mongodb_uri---------------
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mpi5l.mongodb.net/?retryWrites=true&w=majority`;
 
 app.get('/', (req, res) => {
   res.send('Hello Todo app!')
