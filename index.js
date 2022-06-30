13dd47a4038c08a1fd0399d78e539cbb9d4c2af9
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2022-07-01 00:58:30 +0600

    database add

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ app.use(express.json());
 // mongodb_uri---------------
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mpi5l.mongodb.net/?retryWrites=true&w=majority`;
 
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try{
+      await client.connect();
+      
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
   res.send('Hello Todo app!')
 })
